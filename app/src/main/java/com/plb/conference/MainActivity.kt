04fedb5f3b91ca79ab87d2951e68822a6309af76
Jean Paul Jacquot <jeanpauljacquot@admins-MacBook-Pro.local>
2025-01-27 11:16:19 +0100

    initial commit

---
+++ app/src/main/java/com/plb/conference/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.plb.conference
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.plb.conference.ui.theme.ConferenceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ConferenceTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    Row {
+                        OutlinedButton(
+                            onClick= { }
+                        ) {
+                            Text("Button 1")
+                        }
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding) // Add padding
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ConferenceTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column {
+                Greeting(
+                    name = "Android",
+                    modifier = Modifier.padding(innerPadding) // Add padding
+                )
+                Greeting(
+                    name = "iOS",
+                    modifier = Modifier.padding(innerPadding) // Add padding
+                )
+            }
+        }
+    }
+}
